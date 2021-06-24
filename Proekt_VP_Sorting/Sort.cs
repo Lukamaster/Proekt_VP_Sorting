2f784e8b98ab52ac74d3465c141b9e7cd65565bc
Luka Cheshlarov <luka.ceslarov@gmail.com>
2021-06-24 19:25:32 +0200

    Created class Sort

--- Proekt_VP_Sorting/Sort.cs
+++ Proekt_VP_Sorting/Sort.cs
@@ -6,5 +6,21 @@ using System.Threading.Tasks;
 
 namespace Proekt_VP_Sorting {
     class Sort {
+
+        public Random random { get; set; }
+        public int[] array { get; set; }
+
+        public int size { get; set; }
+
+        public int delay { get; set; }
+
+        public Sort(int[] array,int size, int delay) {
+            this.size = size;
+            this.delay = delay;
+
+        }
+
+
+
     }
 }
